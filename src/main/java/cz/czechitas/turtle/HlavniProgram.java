46a6d173bb8e8ed438dea769cb7b948b7a95fdc1
monbira <monikabiras@gmail.com>
2023-02-27 17:04:47 +0100

    test

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -6,8 +6,38 @@ public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
+        zofka.setLocation(100,100);
+        nakreslitrojuhelnik(50);
+        zofka.setLocation(200,200);
+        nakreslictverec(40);
 
     }
+
+    public void nakreslitrojuhelnik (double velikoststrany) {
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(120);
+    }
+
+    public void nakreslictverec (double velikoststrany) {
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+
+    }
+
+    public void nakresliobdelnik (double velikoststrany) {
+
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
