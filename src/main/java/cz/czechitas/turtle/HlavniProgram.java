0ce439b75cf401d035b92e56cf4d015fb0862680
monbira <monikabiras@gmail.com>
2023-02-27 17:45:08 +0100

    test

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -6,36 +6,121 @@ public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
-        zofka.setLocation(100,100);
-        nakreslitrojuhelnik(50);
+        zofka.setLocation(200,200);
+        nakreslitrojuhelnik(300);
         zofka.setLocation(200,200);
         nakreslictverec(40);
+        zofka.setLocation(300,300);
+        nakreslikolecko(300);
 
     }
 
     public void nakreslitrojuhelnik (double velikoststrany) {
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(120);
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(120);
-        zofka.move(100);
+        zofka.move (velikoststrany);
         zofka.turnLeft(120);
     }
 
     public void nakreslictverec (double velikoststrany) {
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(90);
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(90);
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(90);
-        zofka.move(100);
+        zofka.move(velikoststrany);
         zofka.turnLeft(90);
-
     }
 
-    public void nakresliobdelnik (double velikoststrany) {
+    public void nakresliobdelnik (double a, double b) {
+        zofka.move(a);
+        zofka.turnLeft(90);
+        zofka.move(b);
+        zofka.turnLeft(90);
+        zofka.move(a);
+        zofka.turnLeft(90);
+        zofka.move(b);
+        zofka.turnLeft(90);
+    }
 
+
+
+    public void nakreslikolecko (double r) {
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
+        zofka.turnLeft(10);
+        zofka.move(5);
     }
 
 
