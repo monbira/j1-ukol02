cc42fec9cd62f54fdc9d2414e7c9914bc91cf425
monikabiras <monikabiras@gmail.com>
2024-09-16 11:53:48 +0200

    zmrzlina a snehulak

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -8,47 +8,28 @@ public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
+
         zofka.setPenColor(Color.black);
-        zofka.setPenWidth(5);
-
-        zofka.setLocation(150, 100);
-        nakreslikolecko(10);
-        zofka.setLocation(145,120);
-        zofka.turnLeft(90);
-        nakreslitrojuhelnik(108);
-
-        zofka.setLocation(250,70);
-        nakreslikolecko(7);
-        zofka.setLocation(250,150);
-        nakreslikolecko(10);
-        zofka.setLocation(250,265);
-        nakreslikolecko(14);
-        zofka.setLocation(330,175);
-        nakreslikolecko(3);
-        zofka.setLocation(180,175);
-        nakreslikolecko(3);
-
-        zofka.setLocation(900,100);
-        nakresliobdelnik(100,150);
-        zofka.turnLeft(90);
-        zofka.move(150);
-        zofka.turnLeft(180);
-        nakreslikolecko(9);
-        zofka.setLocation(800,250);
-        zofka.move(85);
-        zofka.turnLeft(90);
-        nakresliobdelnik(160,85);
-        zofka.turnLeft(90);
-        zofka.move(85);
+        //zmrzlina
+        zofka.setLocation(100,100);
+        nakresliKolecko(20);
         zofka.turnRight(90);
-        zofka.move(60);
-        nakreslikolecko(4);
-        zofka.move(65);
-        nakreslikolecko(4);
-        zofka.move(35);
-        zofka.turnRight(90);
-        zofka.setLocation(640,280);
-        nakreslitrojuhelnik(90);
+        nakresliTrojuhelnik(120);
+        //snehulak
+        zofka.setLocation(100,300);
+        nakresliKolecko(5);
+        zofka.setLocation(100,330);
+        nakresliKolecko(10);
+        zofka.setLocation(100,390);
+        nakresliKolecko(15);
+        zofka.setLocation(60,340);
+        nakresliKolecko(5);
+        zofka.setLocation(140,340);
+        nakresliKolecko(5);
+        //masinka
+
+
+
 
 
 
@@ -56,117 +37,30 @@ public class HlavniProgram {
 
     }
 
-    public void nakreslitrojuhelnik (double velikoststrany) {
-        zofka.move(velikoststrany);
-        zofka.turnLeft(120);
-        zofka.move(velikoststrany);
-        zofka.turnLeft(120);
-        zofka.move (velikoststrany);
-        zofka.turnLeft(120);
+    private void nakresliTrojuhelnik(int delkaTrojuhelniku) {
+        for (int i = 0; i < 3; i++) {
+            zofka.move(delkaTrojuhelniku);
+            zofka.turnRight(120);
+        }
     }
 
-    public void nakreslictverec (double velikoststrany) {
-        zofka.move(velikoststrany);
-        zofka.turnLeft(90);
-        zofka.move(velikoststrany);
-        zofka.turnLeft(90);
-        zofka.move(velikoststrany);
-        zofka.turnLeft(90);
-        zofka.move(velikoststrany);
-        zofka.turnLeft(90);
+    private void nakresliKolecko(int delkaStranyKolecka) {
+        for (int i = 0; i < 20; i++) {
+            zofka.move(delkaStranyKolecka);
+            zofka.turnRight(18);
+        }
     }
 
-    public void nakresliobdelnik (double a, double b) {
-        zofka.move(a);
-        zofka.turnLeft(90);
-        zofka.move(b);
-        zofka.turnLeft(90);
-        zofka.move(a);
-        zofka.turnLeft(90);
-        zofka.move(b);
-        zofka.turnLeft(90);
+    private void nakresliObdelnik(int delkaObdelniku, int sirkaObdelniku) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(delkaObdelniku);
+            zofka.turnRight(90);
+            zofka.move(sirkaObdelniku);
+            zofka.turnRight(90);
+        }
     }
 
-
-
-    public void nakreslikolecko (double r) {
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-        zofka.turnLeft(10);
-        zofka.move(r);
-    }
-
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         new HlavniProgram().start();
     }
-
 }
